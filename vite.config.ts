1859f09024183ea5b5b9ef31d0b133305e37eca9
sakura-adachi <sakura-adachi@cybozu.co.jp>
2023-10-23 22:24:19 +0900

    🎉init

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,7 @@
-import react from '@vitejs/plugin-react'
-import vike from 'vike/plugin'
-import { UserConfig } from 'vite'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
 
-const config: UserConfig = {
-  plugins: [react(), vike()]
-}
-
-export default config
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+})
