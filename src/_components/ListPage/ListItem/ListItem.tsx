ce16f633eabd93f181a24f7e88a62b70bf2cb707
sakura-adachi <sakura-adachi@cybozu.co.jp>
2023-10-24 22:10:29 +0900

    feat: shad/cn

--- src/_components/ListPage/ListItem/ListItem.tsx
+++ src/_components/ListPage/ListItem/ListItem.tsx
@@ -1,7 +1,9 @@
+import { Button } from '@/_components/ui/button'
 export const ListItem = () => {
   return (
     <>
       <p>this is react template</p>
+      <Button>Button</Button>
     </>
   )
 }
