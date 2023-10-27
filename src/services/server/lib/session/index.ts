1b56918c67b61b81ee01bdddd1ee5c7693bbcef8
shunsuke-tamura <wwwxx4869@gmail.com>
2023-10-27 16:51:22 +0900

    feat: redisの接続先を.envから取得するようにした

--- src/services/server/lib/session/index.ts
+++ src/services/server/lib/session/index.ts
@@ -19,7 +19,9 @@ export const getSession: (
 ) => Promise<GetSessionReturn & AppSession> = nextSession({
   store: promisifyStore(
     new RedisStore({
-      client: new Redis(),
+      client: new Redis(
+        process.env.REDIS_URL ? process.env.REDIS_URL : 'redis://127.0.0.1:6379',
+      ),
     }),
   ),
   cookie: {
