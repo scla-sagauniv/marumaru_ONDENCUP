8aea710c6f067f535eff1dcbbdde1e2f3ce1d31b
shunsuke-tamura <wwwxx4869@gmail.com>
2023-10-29 06:42:11 +0900

    fix: baseURLを環境変数から取得するように

--- src/utils/trpc.ts
+++ src/utils/trpc.ts
@@ -14,6 +14,9 @@ export function getBaseUrl() {
   if (process.env.RENDER_INTERNAL_HOSTNAME)
     // reference for render.com
     return `http://${process.env.RENDER_INTERNAL_HOSTNAME}:${process.env.PORT}`
+  if (process.env.BASE_URL) {
+    return `${process.env.BASE_URL}:${process.env.PORT ?? 8080}}`
+  }
   // assume localhost
   return `http://localhost:${process.env.PORT ?? 8080}`
 }
