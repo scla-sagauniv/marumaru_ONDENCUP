933b296faf23e0eb9ae210fa70f0b892f9ffd6c7
saku-1101 <sakupi-1101@outlook.jp>
2023-10-29 00:19:00 +0900

    fix: fix middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,7 +3,7 @@ import { NextRequest, NextResponse } from 'next/server'
 import { UserOnAppType } from './services/schema/user'
 
 export const config = {
-  matcher: ['/api-demo/:path*', '/todo/:path*'],
+  matcher: ['/todo/:path*', '/user/:path*'],
 }
 
 export async function middleware(request: NextRequest) {
@@ -12,20 +12,22 @@ export async function middleware(request: NextRequest) {
   // だから、Nextの機能使ってcookie取得してfetchで無理やりcookie送るしかないのかな。。。
   const cookie = request.cookies.get('sid')
   const res = await fetch(
-    'http://localhost:8080/api/trpc/auth.fetchUser?batch=1&input={}',
+    `${process.env.BASE_URL}/api/trpc/auth.fetchUser?batch=1&input={}`,
     {
       headers: {
         Cookie: `sid=${cookie?.value}`,
       },
     },
   )
-  const user: UserOnAppType | undefined = (await res.json())[0].result.data.user
+  const user: UserOnAppType | undefined = (await res.json())[0].result.data.json.user
 
-  if (user === undefined) {
+  if (!user) {
     const url = request.nextUrl.clone()
     url.pathname = '/auth'
     return NextResponse.redirect(url)
   } else if (request.url.includes('/confirm/email')) {
+    return NextResponse.redirect(new URL(`/user/board/${user.id}`))
+  } else {
     return NextResponse.redirect(new URL(`/user/board/${user.id}`))
   }
 }
